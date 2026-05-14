8f801e13e15486415c2253b6333f112b3002d7a1
Ale Rodriguez <robertorodri110@gmail.com>
2026-05-13 22:43:11 -0300

    Fix: add uid to Usuario model

--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -5,7 +5,7 @@ class Usuario extends Model {}
 
 Usuario.init({
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  uid: { type: DataTypes.STRING, allowNull: false, unique: true },
+  uid: { type: DataTypes.STRING, allowNull: true, unique: true },
   nombre: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
   rol: { type: DataTypes.ENUM('encargado', 'empleado'), allowNull: false, defaultValue: 'empleado' },
