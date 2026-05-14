1e9845a123747e80ea505e5d85dd0ed2f8b5d5e0
Ale Rodriguez <robertorodri110@gmail.com>
2026-05-13 22:23:51 -0300

    Force sync DB

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ app.get('/api/test', (req, res) => {
 app.use('/api/productos', productosRouter);
 app.use('/api/usuarios', usuariosRouter);
 
-sequelize.sync({ force: true })
+sequelize.sync({ alter: true })
   .then(() => {
     console.log('Base de datos conectada y tablas sincronizadas.');
     app.listen(PORT, () => {
